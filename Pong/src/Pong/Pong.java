757b3bfea26ff5f300acf9fcb811711d9b819ee6
Harlan Kendrick <harlankendrick13@gmail.com>
2026-05-25 10:35:50 +1200

    updated Pong.java with descriptive constants

--- Pong/src/Pong/Pong.java
+++ Pong/src/Pong/Pong.java
@@ -4,15 +4,22 @@ import javax.swing.JFrame;
 
 public class Pong extends JFrame {
 
-		public Pong() {
-			setTitle("Pong");
-			setSize(800,600);
-			setResizable(false);
-			setVisible(true);
-			setDefaultCloseOperation(EXIT_ON_CLOSE);
-		}
+	// -------------------------------
+	// Window configuration constants
+	// -------------------------------
+	private static final String WINDOW_TITLE = "Pong";
+	private static final int WINDOW_WIDTH = 800;
+	private static final int WINDOW_HEIGHT = 600;
+	
+	public Pong() {
+		setTitle(WINDOW_TITLE);
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 		
-		public static void main(String[] args) {
-			new Pong();
-		}
+	public static void main(String[] args) {
+		new Pong();
+	}
 }
