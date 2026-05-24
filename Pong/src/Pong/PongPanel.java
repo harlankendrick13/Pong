c09258a8736db3c24e45a9546a178774370a0d6f
Harlan Kendrick <harlankendrick13@gmail.com>
2026-05-25 11:06:54 +1200

    PongPanel class created and colour contructor added

---
+++ Pong/src/Pong/PongPanel.java
@@ -0,0 +1,43 @@
+package Pong;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+
+public class PongPanel extends JPanel implements ActionListener, KeyListener {
+	
+	// -----------------------------------
+	// Colour Constructor
+	// -----------------------------------
+	public PongPanel(Color backgroundColour) {
+		setBackground(backgroundColour);
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
