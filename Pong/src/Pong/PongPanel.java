b2b7e6fa3a98cf221c21b08ae6f1e86d8b485b96
Harlan Kendrick <harlankendrick13@gmail.com>
2026-05-25 13:25:56 +1200

    creation of Sprite class

--- Pong/src/Pong/PongPanel.java
+++ Pong/src/Pong/PongPanel.java
@@ -9,6 +9,8 @@ import javax.swing.Timer;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.BasicStroke;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	
@@ -33,8 +35,18 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 		super.paintComponent(g);
 		g.setColor(Color.WHITE);
 		g.fillRect(20,  20,  100,  100);
+		paintDottedLine(g);
 	}
 
+	private void paintDottedLine(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g.create();
+			Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] {9}, 0);
+			g2d.setStroke(dashed);
+			g2d.setPaint(Color.WHITE);
+			g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+			g2d.dispose();
+	}
+	
 	@Override
 	public void keyTyped(KeyEvent event) {
 		// TODO Auto-generated method stub
---
+++ Pong/src/Pong/Sprite.java
@@ -0,0 +1,144 @@
+package Pong;
+
+import java.awt.Rectangle;
+import java.awt.Color;
+
+public class Sprite {
+	
+	// Position
+	private int xPosition;
+	private int yPosition;
+	private int initialXPosition;
+	private int initialYPosition;
+	
+	// Velocity
+	private int xVelocity;
+	private int yVelocity;
+	
+	// Size
+	private int width;
+	private int height;
+
+	// Colour
+	private Color colour;
+	
+    // -------------------------
+    // Getters and Setters
+    // -------------------------
+	
+	public int getXPosition() {
+		return xPosition;
+	}
+	
+	public void setXPosition(int xPosition) {
+		this.xPosition = xPosition;
+	}
+	
+	public int getYPosition() {
+		return yPosition;
+	}
+	
+	public void setYPosition(int yPosition) {
+		this.yPosition = yPosition;
+	}
+	
+	public int getXVelocity() {
+		return xVelocity;
+	}
+	
+	public void setXVelocity(int xVelocity) {
+		this.xVelocity = xVelocity;
+	}
+	
+	public int getYVelocity() {
+		return yVelocity;
+	}
+	
+	public void setYVelocity(int yVelocity) {
+		this.yVelocity = yVelocity;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	public Color getColour() {
+		return colour;
+	}
+	
+	public void setColour(Color colour) {
+		this.colour = colour;
+	}
+	
+	public void setInitialPosition(int initialX, int initialY) {
+		this.initialXPosition = initialX;
+		this.initialYPosition = initialY;
+	}
+	
+	public void resetToInitialPosition() {
+		this.xPosition = initialXPosition;
+		this.yPosition = initialYPosition;
+	}
+	
+	// Existing simple setters
+	public void setXPosition(int newX) {
+		xPosition = newX;
+	}
+	
+	public void setYPosition(int newY) {
+		yPosition = newY;
+	}
+	
+	// overloaded setters with bounds checking
+	
+	public void seXPosition(int newX, int panelWidth) {
+		xPosition = newX;
+		
+		// Left bound
+		if (xPosition <0) {
+			xPosition = 0;
+		}
+		// Right bound
+		else if (xPosition + width > panelWidth) {
+			xPosition = panelWidth - width;
+		}
+	}
+	
+	public void setYPosition(int newY, int panelHeight) {
+		yPosition = newY;
+		
+		// Top bound
+		if (yPosition < 0) {
+			yPosition = 0;
+		}
+		// Bottom bound
+		else if (yPosition + height > panelHeight) {
+			yPosition = panelHeight - height;
+		}
+	}
+	
+	public Rectangle getRectangle() {
+		return new Rectangle(getXPosition(), getYPosition(), getWidth(), getHeight());
+	}
+}
+
+
+
+
+
+
+
+
+
